5709ea4754177ca97cbc3d64b32dae696ff5f82b
Aliraza-123 <Aliraza.infini8ai@gmail.com>
2022-04-28 21:16:39 +0500

    code fetched from dev_zohair  branch

--- app/src/main/java/com/example/fitnessfirst/login/SignUpActivity.java
+++ app/src/main/java/com/example/fitnessfirst/login/SignUpActivity.java
@@ -1,8 +1,10 @@
 package com.example.fitnessfirst.login;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -12,30 +14,64 @@ import com.example.fitnessfirst.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    EditText edtEmail, edtFirstName, edtLastName, edtPassword, edtConfirmPassword;
+    Button btnLogin;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        findViewById(R.id.txt_signin).setOnClickListener(new View.OnClickListener() {
+        init();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent =  new Intent(SignUpActivity.this,LoginActivity.class);
-                startActivity(intent);
+                if(validation()){
+                    Toast.makeText(SignUpActivity.this, "validation true", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
-//        setupToolbar();
-    }
-    private void setupToolbar() {
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        toolbar.setNavigationIcon(androidx.appcompat.R.drawable.abc_ic_ab_back_material);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
     }
+    private void init(){
+        edtEmail =  findViewById(R.id.edt_email);
+        edtFirstName = findViewById(R.id.edt_first_name);
+        edtLastName =  findViewById(R.id.edt_last_name);
+        edtPassword =  findViewById(R.id.edt_password);
+        edtConfirmPassword =  findViewById(R.id.edt_confirm_password);
+        btnLogin = findViewById(R.id.btn_login);
+    }
+    private boolean validation(){
+        if(edtEmail.getText().toString().isEmpty()){
+            edtEmail.setError("email is empty");
+            return false;
+        }
+        if(edtFirstName.getText().toString().isEmpty()){
+            edtFirstName.setError("firstName is empty");
+            return false;
+        }
+        if(edtLastName.getText().toString().isEmpty()){
+            edtLastName.setError("lastName is empty");
+            return false;
+        }
+        if(edtPassword.getText().toString().isEmpty()){
+            edtPassword.setError("password is empty");
+            return false;
+        }
+        if(edtConfirmPassword.getText().toString().isEmpty()){
+            edtConfirmPassword.setError("confirm password is empty");
+            return false;
+        }
+        if(!edtPassword.getText().toString().trim().equalsIgnoreCase(edtConfirmPassword.getText().toString().trim())){
+            Toast.makeText(this, "Password and Confirm password is not matched", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+
 }
