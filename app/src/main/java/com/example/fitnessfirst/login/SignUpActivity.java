14b5889a934470a54b7a06b0c63814a6c611d8ce
Muhammad Zohair <zohair739@gmail.com>
2022-05-04 15:47:51 +0500

    signup completed

--- app/src/main/java/com/example/fitnessfirst/login/SignUpActivity.java
+++ app/src/main/java/com/example/fitnessfirst/login/SignUpActivity.java
@@ -1,14 +1,13 @@
 package com.example.fitnessfirst.login;
 
-import android.content.Intent;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import com.example.fitnessfirst.R;
 import com.example.fitnessfirst.repository.FirebaseDatabaseHelper;
@@ -16,86 +15,105 @@ import com.example.fitnessfirst.repository.User;
 import com.example.fitnessfirst.utils.Utils;
 
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText edtEmail, edtFirstName, edtLastName, edtPassword, edtConfirmPassword;
-    Button btnLogin;
-
-
+    private EditText edtEmail, edtFirstName, edtLastName, edtPassword, edtConfirmPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
         init();
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(validation()){
-                    User user =  new User();
-                    user.setEmailAddress(edtEmail.getText().toString().trim());
-                    user.setFirstName(edtFirstName.getText().toString().trim());
-                    user.setLastName(edtLastName.getText().toString().trim());
-                    user.setPassword(edtPassword.getText().toString().trim());
-                    if( FirebaseDatabaseHelper.createUser(user,null)){
-                        Intent intent =  new Intent(SignUpActivity.this,LoginActivity.class);
-                        startActivity(intent);
-                    }
-                    else{
-                        Toast.makeText(SignUpActivity.this, "Some issue!!", Toast.LENGTH_SHORT).show();
-
-                    }
-
-                }
-
-            }
-        });
 
     }
-    private void init(){
-        edtEmail =  findViewById(R.id.edt_email);
+
+    private void init() {
+        edtEmail = findViewById(R.id.edt_email);
+
         edtFirstName = findViewById(R.id.edt_first_name);
-        edtLastName =  findViewById(R.id.edt_last_name);
-        edtPassword =  findViewById(R.id.edt_password);
-        edtConfirmPassword =  findViewById(R.id.edt_confirm_password);
+
+        edtLastName = findViewById(R.id.edt_last_name);
+
+        edtPassword = findViewById(R.id.edt_password);
+
+        edtConfirmPassword = findViewById(R.id.edt_confirm_password);
+
         btnLogin = findViewById(R.id.btn_login);
-    }
-    private boolean validation(){
-        if(edtEmail.getText().toString().isEmpty()){
-            edtEmail.setError("email is empty");
-            return false;
-        }
-        if(!Utils.validateEmail(edtEmail.getText().toString())){
-            return false;
-        }
-
-        if(edtFirstName.getText().toString().isEmpty()){
-            edtFirstName.setError("firstName is empty");
-            return false;
-        }
-        if(edtLastName.getText().toString().isEmpty()){
-            edtLastName.setError("lastName is empty");
-            return false;
-        }
-        if(edtPassword.getText().toString().isEmpty()||edtPassword.getText().toString().length()<6){
-            edtPassword.setError("password is empty");
-            return false;
-        }
-        if( (Utils.validatePassword(edtPassword.getText().toString()))){
-            return false;
-        }
-
-        if(edtConfirmPassword.getText().toString().isEmpty()){
-            edtConfirmPassword.setError("confirm password is empty");
-            return false;
-        }
-        if(!edtPassword.getText().toString().trim().equalsIgnoreCase(edtConfirmPassword.getText().toString().trim())){
-            Toast.makeText(this, "Password and Confirm password is not matched", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-
-        return true;
+        btnLogin.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        if (view == btnLogin) {
 
+            String email = edtEmail.getText().toString();
+            String password = edtPassword.getText().toString();
+            String confirmPassword = edtConfirmPassword.getText().toString();
+            String firstName = edtFirstName.getText().toString();
+            String lastName = edtLastName.getText().toString();
+
+            if (email.isEmpty()) {
+                edtEmail.setError("Email is empty");
+                return;
+            }
+            if (!Utils.validateEmail(email)) {
+                edtEmail.setError("Invalid Email");
+                return;
+            }
+
+            if (firstName.isEmpty()) {
+                edtFirstName.setError("FirstName is empty");
+                return;
+            }
+
+            if (lastName.isEmpty()) {
+                edtLastName.setError("LastName is empty");
+                return;
+            }
+
+            if (password.isEmpty()) {
+                edtPassword.setError("Password is empty");
+                return;
+            }
+
+            if (confirmPassword.isEmpty()) {
+                edtConfirmPassword.setError("confirm password is empty");
+                return;
+            }
+
+            if (password.length() < 6 || confirmPassword.length() < 6) {
+                edtPassword.setError("Password length should be more than 6 characters");
+                return;
+            }
+
+            if (!password.trim().equals(confirmPassword.trim())) {
+                Utils.showToast(this, "Passwords doesn't match");
+                edtPassword.setText("");
+                edtConfirmPassword.setText("");
+                return;
+            }
+
+            ProgressDialog progressDialog = new ProgressDialog(SignUpActivity.this);
+            progressDialog.setTitle("Account creation in progress...");
+            progressDialog.show();
+
+            User user = new User(Utils.PUBLIC_USER_TYPE, "", firstName, lastName, email, password);
+
+            FirebaseDatabaseHelper.createUser(user, null);
+            FirebaseDatabaseHelper.getAllPublicUsers().add(user);
+
+            new Handler().postDelayed(() -> {
+                edtEmail.setText(null);
+                edtConfirmPassword.setText(null);
+                edtLastName.setText(null);
+                edtPassword.setText(null);
+                edtFirstName.setText(null);
+                progressDialog.dismiss();
+
+                Utils.showToast(getApplicationContext(), "Account created successfully");
+            }, 3000);
+        }
+    }
 }
--- app/src/main/java/com/example/fitnessfirst/utils/Utils.java
+++ app/src/main/java/com/example/fitnessfirst/utils/Utils.java
@@ -1,30 +1,33 @@
 package com.example.fitnessfirst.utils;
 
-import android.app.Activity;
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.preference.PreferenceManager;
-import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Locale;
-import java.util.Objects;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
+/**
+ * The type Helper class.
+ */
 public class Utils {
 
+    /**
+     * The constant PUBLIC_USER_TYPE.
+     */
+    public static final String PUBLIC_USER_TYPE = "PUBLIC_USER";
+
+    /**
+     * The constant ADMIN_TYPE.
+     */
+    public static final String ADMIN_TYPE = "ADMIN";
 
     private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
@@ -32,21 +35,45 @@ public class Utils {
     private static final Pattern VALID_PASSWORD_REGEX =
             Pattern.compile("^(?=.*[0-9])(?=.*[a-z]).{6,}$", Pattern.CASE_INSENSITIVE);
 
+    /**
+     * Validate email boolean.
+     *
+     * @param emailStr the email str
+     * @return the boolean
+     */
     public static boolean validateEmail(CharSequence emailStr) {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
         return matcher.find();
     }
 
+    /**
+     * Validate password boolean.
+     *
+     * @param passwordStr the password str
+     * @return the boolean
+     */
     public static boolean validatePassword(CharSequence passwordStr) {
         Matcher matcher = VALID_PASSWORD_REGEX.matcher(passwordStr);
         return matcher.find();
     }
 
+    /**
+     * Generate uuid string.
+     *
+     * @return the string
+     */
     public static String generateUUID() {
         UUID uuid = UUID.randomUUID();
         return uuid.toString();
     }
 
+    /**
+     * Load image.
+     *
+     * @param context   the context
+     * @param userId    the user id
+     * @param imageView the image view
+     */
     public static void loadImage(Context context, String userId, ImageView imageView) {
         final StorageReference mStorageRef = FirebaseStorage.getInstance().getReference()
                 .child("user_profile_pictures/" + userId + ".png");
@@ -58,6 +85,12 @@ public class Utils {
         });
     }
 
+    /**
+     * Upload image.
+     *
+     * @param id       the id
+     * @param imageUri the image uri
+     */
     public static void uploadImage(String id, Uri imageUri) {
         final StorageReference mStorageRef = FirebaseStorage.getInstance().getReference()
                 .child("user_profile_pictures/" + id + ".png");
@@ -65,132 +98,46 @@ public class Utils {
         mStorageRef.putFile(imageUri);
     }
 
-    public static void getDate(Activity context, EditText editText) {
-        Calendar myCalendar = Calendar.getInstance();
-
-
-        DatePickerDialog.OnDateSetListener date = (view, year, monthOfYear, dayOfMonth) -> {
-            myCalendar.set(Calendar.YEAR, year);
-            myCalendar.set(Calendar.MONTH, monthOfYear);
-            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-            String myFormat = "dd-MM-yyyy";
-            SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-
-            String startDateStr = sdf.format(myCalendar.getTime());
-            editText.setText(startDateStr);
-
-        };
-        new DatePickerDialog(Objects.requireNonNull(context), date, myCalendar
-                .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-    }
-
-    public static void getTime(Context context, EditText editText) {
-        Calendar mCurrentTime = Calendar.getInstance();
-        int hour = mCurrentTime.get(Calendar.HOUR_OF_DAY);
-        int minute = mCurrentTime.get(Calendar.MINUTE);
-        TimePickerDialog mTimePicker;
-        mTimePicker = new TimePickerDialog(context, (timePicker, hourOfDay, minute1) -> {
-            boolean isPM = (hourOfDay >= 12);
-            String time = " " + String.format("%02d:%02d %s",
-                    (hourOfDay == 12 || hourOfDay == 0) ? 12 : hourOfDay % 12, minute1, isPM ? "PM" : "AM");
-            editText.setText(time);
-        }, hour, minute, false);
-        mTimePicker.setTitle("Select Time");
-        mTimePicker.show();
-    }
-
-    public static String formulateDate(String date) {
-        StringBuilder dateBuilder = new StringBuilder();
-
-        int day = Integer.parseInt(date.substring(0, 2));
-        int month = Integer.parseInt(date.substring(3, 5));
-        int year = Integer.parseInt(date.substring(6));
-        Calendar calendar = new GregorianCalendar(year, (month), (day - 1));
-
-        dateBuilder.append(day + " ");
-
-        switch (month) {
-            case 1:
-                dateBuilder.append("JANUARY ");
-                break;
-            case 2:
-                dateBuilder.append("FEBRUARY ");
-                break;
-            case 3:
-                dateBuilder.append("MARCH ");
-                break;
-            case 4:
-                dateBuilder.append("APRIL ");
-                break;
-            case 5:
-                dateBuilder.append("MAY ");
-                break;
-            case 6:
-                dateBuilder.append("JUNE ");
-                break;
-            case 7:
-                dateBuilder.append("JULY ");
-                break;
-            case 8:
-                dateBuilder.append("AUGUST ");
-                break;
-            case 9:
-                dateBuilder.append("SEPTEMBER ");
-                break;
-            case 10:
-                dateBuilder.append("OCTOBER ");
-                break;
-            case 11:
-                dateBuilder.append("NOVEMBER ");
-                break;
-            case 12:
-                dateBuilder.append("DECEMBER ");
-                break;
-        }
-
-        dateBuilder.append(year + ", ");
-
-        int daySwitch = calendar.get(Calendar.DAY_OF_WEEK);
-
-        switch (daySwitch) {
-            case Calendar.MONDAY:
-                dateBuilder.append("MONDAY");
-                break;
-            case Calendar.TUESDAY:
-                dateBuilder.append("TUESDAY");
-                break;
-            case Calendar.WEDNESDAY:
-                dateBuilder.append("WEDNESDAY");
-                break;
-            case Calendar.THURSDAY:
-                dateBuilder.append("THURSDAY");
-                break;
-            case Calendar.FRIDAY:
-                dateBuilder.append("FRIDAY");
-                break;
-            case Calendar.SATURDAY:
-                dateBuilder.append("SATURDAY");
-                break;
-            case Calendar.SUNDAY:
-                dateBuilder.append("SUNDAY");
-                break;
-
-        }
-        return dateBuilder.toString();
-    }
-
+    /**
+     * Save data in shared prefs.
+     *
+     * @param context the context
+     * @param key     the key
+     * @param value   the value
+     */
     public static void saveDataInSharedPrefs(Context context, String key, String value) {
         PreferenceManager.getDefaultSharedPreferences(context).edit().putString(key, value).apply();
     }
 
+    /**
+     * Gets data from shared prefs.
+     *
+     * @param context the context
+     * @param key     the key
+     * @return the data from shared prefs
+     */
     public static String getDataFromSharedPrefs(Context context, String key) {
         return PreferenceManager.getDefaultSharedPreferences(context).getString(key, "");
     }
 
+    /**
+     * Clear shared prefs.
+     *
+     * @param context the context
+     */
     public static void clearSharedPrefs(Context context) {
         PreferenceManager.getDefaultSharedPreferences(context).edit().putString("email", "").apply();
         PreferenceManager.getDefaultSharedPreferences(context).edit().putString("password", "").apply();
         PreferenceManager.getDefaultSharedPreferences(context).edit().putString("type", "").apply();
+    }
+
+    /**
+     * Success.
+     *
+     * @param context the context
+     * @param message the message
+     */
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 }
